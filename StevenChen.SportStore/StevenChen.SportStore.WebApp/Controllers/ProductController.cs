0b7207b39d2a4ed708073b02bb21307aae451f89
hongjun <hongjunmontreal@gmail.com>
2019-03-30 18:01:08 -0400

    1[new] add productControl
    2[new] add view List

---
+++ StevenChen.SportStore/StevenChen.SportStore.WebApp/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using StevenChen.SportStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StevenChen.SportStore.WebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductsRepository repository;
+        public ProductController(IProductsRepository productsRepository)
+        {
+            this.repository = productsRepository;
+        }
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+    }
+}
