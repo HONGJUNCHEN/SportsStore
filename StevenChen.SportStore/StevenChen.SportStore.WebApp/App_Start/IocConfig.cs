c6ab251853f6d490cff8861a3979c07b2745c41f
hongjun <hongjunmontreal@gmail.com>
2019-03-30 17:34:32 -0400

    1[new] create IocConfig
    2[new] Autofac dependency Resolver

---
+++ StevenChen.SportStore/StevenChen.SportStore.WebApp/App_Start/IocConfig.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StevenChen.SportStore.WebApp
+{
+    public class IocConfig
+    {
+       public static void Register()
+        {
+            var builder = new ContainerBuilder();
+
+            // Register your MVC controllers. (MvcApplication is the name of
+            // the class in Global.asax.)
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+           
+
+            // Set the dependency resolver to be Autofac.
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+
+    }
+}
--- StevenChen.SportStore/StevenChen.SportStore.WebApp/Global.asax.cs
+++ StevenChen.SportStore/StevenChen.SportStore.WebApp/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StevenChen.SportStore.WebApp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +14,7 @@ namespace StevenChen.SportStore.WebApp
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            IocConfig.Register();
         }
     }
 }
