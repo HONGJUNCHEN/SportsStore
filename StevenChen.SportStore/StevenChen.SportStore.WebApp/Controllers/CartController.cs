27ba2fea3708549440cc60070f842bc69e5ac03f
hongjun <hongjunmontreal@gmail.com>
2019-04-13 15:43:05 -0400

    [chg] add CartController

---
+++ StevenChen.SportStore/StevenChen.SportStore.WebApp/Controllers/CartController.cs
@@ -0,0 +1,61 @@
+﻿using StevenChen.SportStore.Domain.Abstract;
+using StevenChen.SportStore.Domain.Entities;
+using StevenChen.SportStore.WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StevenChen.SportStore.WebApp.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductsRepository repository;
+        public CartController(IProductsRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+
+        public RedirectToRouteResult AddToCart(int productId, string returnUrl)
+        {
+            Product product = repository
+            .Products
+            .FirstOrDefault(p => p.ProductId == productId);
+            if (product != null)
+            {
+                GetCart().AddItem(product, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
+        {
+            Product product = repository
+            .Products
+            .FirstOrDefault(p => p.ProductId == productId);
+            if (product != null)
+            {
+                GetCart().RemoveLine(product);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+        private Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+    }
+}
---
+++ StevenChen.SportStore/StevenChen.SportStore.WebApp/Models/CartIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using StevenChen.SportStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StevenChen.SportStore.WebApp.Models
+{
+    public class CartIndexViewModel
+    {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
